59abf18a2b8cbc7e5aca7020f5d69766a6f38e38
Maurizio De Marchis <mauriziodm91@gmail.com>
2022-10-29 14:02:18 -0300

    connected to mongoDB and created user model

---
+++ server/models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  name: { type: String, min: 2, max: 50, required: true },
+  email: { type: String, min: 5, max: 50, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, default: '' },
+})
+
+const User = mongoose.model('users', userSchema)
+
+module.exports = User
--- server/server.js
+++ server/server.js
@@ -3,10 +3,16 @@ const app = require('./app')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const server = http.createServer(app)
+const mongoose = require('mongoose')
 
-function startServer() {
+async function startServer() {
   //connect to mongodb
-  server.listen(port, () => console.log('Server is listening on port:', port))
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECT)
+    server.listen(port, () => console.log('Server is listening on port:', port))
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 startServer()
